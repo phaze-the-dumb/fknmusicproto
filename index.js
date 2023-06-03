592a11e0895ce6dd0a62d5553887cc169ee77e70
phaze-the-dumb <phaze@duck.com>
2023-06-03 15:35:10 +0100

    heh

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const MusicMetaServer = require('./server');
 
 let s = new MusicMetaServer({ debug: true });
 
-s.on('ActivityUpdated', a => console.log(s.activity, a.activity.length));
+s.on('ActivityUpdated', a => console.log(s.activity, s.activity.length));
